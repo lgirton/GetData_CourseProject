02251830104fc103ab446bcecca2bc4f53a0e08e
lgirton <lgirton23@gmail.com>
2015-06-18 19:39:04 -0700

    Updated comments

--- run_analysis.R
+++ run_analysis.R
@@ -31,30 +31,41 @@ activities <- read.table("./UCI HAR Dataset/activity_labels.txt",
 # datasets are the same, so this is a utility function to consolidate the 
 # functionality in one place
 getdata <- function(datafile, subjfile, lablfile) {
+  
+  # Read the data file
   data <- read.table(datafile, header = F, colClasses = rep("numeric", length(features)))
+  
+  # Reduce columns to those with mean and std
   data <- data[features_idx]
   colnames(data) <- features[features_idx]
   
+  # Read the subjects
   subj <- read.table(subjfile, header = F, col.names = "subject")
   
+  # Read the labels
   labl <- read.table(lablfile, header = F, col.names = "activity")
   labl$activity <- factor(labl$activity, levels=activities$idx, 
                           labels=activities$activity)
   
+  # Combine columns into one dataset
   data <- bind_cols(subj, data, labl)
 
   return(data)
-  
 }
 
+# Load train dataset with combined labels and subjects
 train <- getdata("./UCI HAR Dataset/train/X_train.txt", 
                  "./UCI HAR Dataset/train/subject_train.txt", 
                  "./UCI HAR Dataset/train/y_train.txt")
 
+# Load test dataset with combined labels and subjects
 test <- getdata("./UCI HAR Dataset/test/X_test.txt", 
                 "./UCI HAR Dataset/test/subject_test.txt", 
                 "./UCI HAR Dataset/test/y_test.txt")
 
+# Combine train and test dataset, gather (long) the measurement columns,
+# summarise mean value by subject, activity and measurement and 
+# then spread (wide) the data for a more condensed output.
 data <- train %>%
   bind_rows(test) %>%
   gather(measurement, value, -subject, -activity) %>%
